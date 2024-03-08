71a003b7cb9b1581abbe99241ffc0a5b1bfd06a5
gabrielokura <gabriellevicz@gmail.com>
2024-03-08 19:12:31 -0300

    Initial Commit

---
+++ native_with_flutter/native_with_flutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_with_flutterApp.swift
+//  native_with_flutter
+//
+//  Created by Gabriel Motelevicz Okura on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct native_with_flutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
